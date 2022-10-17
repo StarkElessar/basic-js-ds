444deaf82029b5e5e9c6adf10245f0de87534cd3
Stark <serj.elessar@gmail.com>
2022-10-17 23:06:12 +0300

    feat: create privat method for min/max node

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,11 +1,11 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
-const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js')
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
   root = null
 
@@ -17,13 +17,13 @@ class BinarySearchTree {
     return {
       data,
       left: null,
-      right: null
+      right: null,
     }
   }
   _isObject(data) {
     if (typeof data !== 'object') return false
     const trueKeys = new Set(Object.keys(data))
-    
+
     return trueKeys.has('data') && trueKeys.has('left') && trueKeys.has('right')
   }
   _addElement(node, data) {
@@ -31,18 +31,26 @@ class BinarySearchTree {
     if (node.data === data) return node
 
     data < node.data
-      ? node.left = this._addElement(node.left, data)
-      : node.right = this._addElement(node.right, data)
-        
+      ? (node.left = this._addElement(node.left, data))
+      : (node.right = this._addElement(node.right, data))
+
     return node
   }
   _hasElementInside(node, data) {
     if (!node) return false
     if (node.data === data) return true
-    
+
     return data < node.data
       ? this._hasElementInside(node.left, data)
       : this._hasElementInside(node.right, data)
+  }
+  _findElement(node, data) {
+    if (!node) return null
+    if (node.data === data) return node
+
+    return data < node.data
+      ? _findElement(node.left, data)
+      : _findElement(node.right, data)
   }
 
   root() {
@@ -57,22 +65,36 @@ class BinarySearchTree {
   }
 
   find(data) {
-    
+    return this._findElement(this.root, data)
   }
 
-  remove(data) {
-    
-  }
+  remove(data) {}
 
   min() {
-    
+    if (!this._root) return
+
+    let node = this._root
+
+    while (node.left) {
+      node = node.left
+    }
+
+    return node.data
   }
 
   max() {
-    
+    if (!this._root) return
+
+    let node = this._root
+
+    while (node.right) {
+      node = node.right
+    }
+
+    return node.data
   }
 }
 
 module.exports = {
-  BinarySearchTree
-};
+  BinarySearchTree,
+}
