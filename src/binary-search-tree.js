a7f9d1dd384c7ce9474afa4c76a9440bf93c1af6
Stark <serj.elessar@gmail.com>
2022-10-17 23:11:48 +0300

    fix: remove public methods

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,25 +7,10 @@ const { Node } = require('../extensions/list-tree.js')
  * using Node from extensions
  */
 class BinarySearchTree {
-  root = null
-
   constructor() {
     this.root = null
   }
-  // public methods
-  _createObject(data) {
-    return {
-      data,
-      left: null,
-      right: null,
-    }
-  }
-  _isObject(data) {
-    if (typeof data !== 'object') return false
-    const trueKeys = new Set(Object.keys(data))
-
-    return trueKeys.has('data') && trueKeys.has('left') && trueKeys.has('right')
-  }
+  // public methods:
   _addElement(node, data) {
     if (!node) return new Node(data)
     if (node.data === data) return node
